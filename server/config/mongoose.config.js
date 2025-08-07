7db29ba1ac7463afba1cc7b517a9052b6740d236
Michael <melomb@students.nu-moa.edu.ph>
2025-08-08 01:57:37 +0800

    aw

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://brailliant_db:<brailliant_db>@cluster0.5tlakc1.mongodb.net/brailliant_db?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Established a connection to the database'))
+    .catch(err => console.log('Something went wrong WEH WEH WEH', err));
+
---
+++ server/index.js
@@ -0,0 +1,89 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express();
+const port = 8000;
+
+const fs = require('fs');
+const path = require('path');
+
+const pdfParse = require('pdf-parse')
+
+const nodemailer = require('nodemailer')
+
+// const helmet = require('helmet');
+// const xss = require('xss-clean');
+// const mongoSanitize = require('express-mongo-sanitize');
+
+// app.use(helmet());             
+// app.use(xss());               
+// app.use(mongoSanitize());  
+
+const louPath = `"C:\\Users\\micha\\Downloads\\liblouis-3.34.0-win64\\bin\\lou_translate.exe"`;
+const table = `"C:\\Users\\micha\\Downloads\\liblouis-3.34.0-win64\\share\\liblouis\\tables\\en-us-g2.ctb"`;
+const tempTxtPath = "C:\\Users\\micha\\Downloads\\test.txt"; // Example input
+const brfFilePath = "C:\\Users\\micha\\Downloads\\output.brf"; // Example output
+
+const cmd = `${louPath} ${table} "${tempTxtPath}" > "${brfFilePath}"`;
+
+
+
+
+
+
+app.use('/files', express.static("files"))
+
+app.use(express.json());
+
+require("./config/mongoose.config")
+
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+
+const AllUserRoutes = require('./routes/user.route')
+AllUserRoutes(app)
+
+const AllBookRoutes = require('./routes/book.route')
+AllBookRoutes(app)
+
+const AllAdminRoutes = require('./routes/admin.route')
+AllAdminRoutes(app)
+
+const AllAuditTrailRoutes = require('./routes/audit_trail.route')
+AllAuditTrailRoutes(app)
+
+const AllSectionRoutes = require('./routes/section.route')
+AllSectionRoutes(app)
+
+const allStudentRoutes = require('./routes/student.route')
+allStudentRoutes(app)
+
+const allRequestBookRoutes = require('./routes/request_book.route')
+allRequestBookRoutes(app)
+
+const AuthRoutes = require('./routes/login.route');
+AuthRoutes(app);
+
+const ArduinoRoutes = require('./routes/arduino.route');
+ArduinoRoutes(app);
+
+const EmailRoutes = require('./routes/email.route');
+EmailRoutes(app);
+
+
+app.listen(port, () => {
+    console.log('server is running', port)
+})
+
+app.get("/", async (req, res) => {
+    res.json("Hello")
+})
+
+
+
+
+
+
+
